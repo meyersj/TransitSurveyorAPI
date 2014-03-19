52c2105343593f3258eecdc1ede048961e2cb2df
jeff <meyers2@pdx.edu>
2014-03-19 00:28:13 +0000

    changes

--- app/app.py
+++ app/app.py
@@ -10,14 +10,13 @@ from logging import FileHandler
 
 base_url = "http://54.244.253.136"
 
-
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+
 handler = FileHandler('/tmp/app.log')
 handler.setLevel(logging.DEBUG)
 app.logger.addHandler(handler)
-
 
 class OnScan(db.Model):
     #TODO make stop_id reference tm_stops
@@ -107,6 +106,8 @@ class Stops(db.Model):
    __tablename__ = 'tm_stops'
    gid = db.Column(db.Integer, primary_key = True)
    rte = db.Column(db.SmallInteger)
+   rte_desc = db.Column(db.Text)
+
    dir = db.Column(db.SmallInteger)
    stop_seq = db.Column(db.Integer)
    stop_id = db.Column(db.Integer)
@@ -115,6 +116,15 @@ class Stops(db.Model):
    def __repr__(self):
        return '<uuid: %r>' % self.stop_id
 
+class Routes(db.Model):
+   __tablename__ = 'tm_routes'
+   gid = db.Column(db.Integer, primary_key = True)
+   rte = db.Column(db.SmallInteger)
+   dir = db.Column(db.SmallInteger)
+   rte_desc = db.Column(db.Text)
+   geom = db.Column(Geometry(geometry_type='MULTILINESTRING', srid=2913))
+   def __repr__(self):
+       return '<uuid: %r>' % self.rte
 
 db.create_all()
 
@@ -184,14 +194,22 @@ def post():
 def stats():
     on_results = OnScan.query.order_by(OnScan.line).all()
     off_results = OffScan.query.order_by(OffScan.line).all()
-
+    routes = db.session.query(Stops.rte, Stops.rte_desc).distinct().all()
+   
     lines = []
-    for row in on_results:
-        lines.append(int(row.line))
-    for row in off_results:
-        lines.append(int(row.line))
+    if routes:
+        for route in routes:
+            lines.append((route.rte, route.rte_desc))
 
-    lines = sorted(list(set(lines)))
+    lines = sorted(lines, key=lambda x: int(x[0]))
+
+    #lines = []
+    #for row in on_results:
+    #    lines.append(int(row.line))
+    #for row in off_results:
+    #    lines.append(int(row.line))
+
+    #lines = sorted(list(set(lines)))
     return render_template('lines.html', base_url=base_url, lines=lines)
 
 
@@ -222,15 +240,25 @@ def home():
 def map():
     geojson = {'type': 'FeatureCollection','features': []}
 
-    stops = db.session.query(func.ST_AsGeoJSON(func.ST_Transform(Stops.geom, 4326),10)).filter_by(rte=12,dir=0)
+    stops = db.session.query(func.ST_AsGeoJSON(func.ST_Transform(Stops.geom, 4326),10)).filter_by(rte=12,dir=0).all()
 
     for stop in stops:
         geom = ast.literal_eval(str(stop[0]))
+        app.logger.error(stop.geom)
         feature_out = {'type':'Feature','geometry':geom,'properties':{'rte': '0'}}
         geojson['features'].append(feature_out)
-        app.logger.debug(geojson)
     return render_template('map.html', base_url=base_url, points=geojson)
 
+@app.route('/map/<rte>')
+def map_route(rte):
+    geojson = {'type': 'FeatureCollection','features': []}
+    route = db.session.query(Routes.rte, Routes.rte_desc, func.ST_AsGeoJSON(func.ST_Transform(Routes.geom, 4326),10).label('geom')).filter_by(rte=rte).first()
+
+    if route:
+        feature_out = {'type':'Feature','geometry':ast.literal_eval(str(route.geom)),'properties':{'rte':route.rte_desc}}
+        geojson['features'].append(feature_out)
+    
+    return render_template('map.html', base_url=base_url, points=geojson)
 
 
 if __name__ == '__main__':
