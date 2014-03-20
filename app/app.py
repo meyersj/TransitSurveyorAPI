425bb29a3190df42758eeaf753c4ae7a52ebdc3a
jeff <meyers2@pdx.edu>
2014-03-20 00:40:15 +0000

    refactor post from app

--- app/app.py
+++ app/app.py
@@ -19,6 +19,7 @@ handler.setLevel(logging.DEBUG)
 app.logger.addHandler(handler)
 
 class OnScan(db.Model):
+    __tablename__ = 'on_scan'
     #TODO make stop_id reference tm_stops
     id = db.Column(db.Integer, primary_key = True)
     uuid = db.Column(db.Text)
@@ -30,8 +31,9 @@ class OnScan(db.Model):
     match = db.Column(db.Boolean)
     geom = db.Column(Geometry(geometry_type='POINT', srid=2913))
     stop_id = db.Column(db.Integer)
+    dist = db.Column(db.Float)
 
-    def __init__(self, uuid, date, line, dir, lon, lat, geom, stop_id):
+    def __init__(self, uuid, date, line, dir, lon, lat, geom, stop_id, dist):
         self.uuid = uuid
         self.date = date
         self.line = line
@@ -40,11 +42,14 @@ class OnScan(db.Model):
         self.lat = lat
         self.match = False
         self.geom = geom
-        self.stop_id = stop_id    
+        self.stop_id = stop_id
+        self.dist = dist
+    
     def __repr__(self):
         return '<uuid: %r>' % self.id
 
 class OffScan(db.Model):
+    __tablename__ = 'off_scan'
     #TODO make stop_id reference tm_stops
     id = db.Column(db.Integer, primary_key = True)
     uuid = db.Column(db.Text)
@@ -55,9 +60,9 @@ class OffScan(db.Model):
     lat = db.Column(db.Text)
     match = db.Column(db.Boolean)
     geom = db.Column(Geometry(geometry_type='POINT', srid=2913))
-    stop_id = db.Column(db.Integer)
-
-    def __init__(self, uuid, date, line, dir, lon, lat, match, geom, stop_id):
+    dist = db.Column(db.Float)
+    
+    def __init__(self, uuid, date, line, dir, lon, lat, match, geom, stop_id, dist):
         self.uuid = uuid
         self.date = date
         self.line = line
@@ -67,6 +72,7 @@ class OffScan(db.Model):
         self.match = match
         self.geom = geom
         self.stop_id = stop_id
+        self.dist = dist
 
     def __repr__(self):
         return '<uuid: %r>' % self.id
@@ -76,31 +82,34 @@ class OnOff_Pairs(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     line = db.Column(db.Text)
     dir = db.Column(db.Text)
-    on_date = db.Column(db.DateTime)
-    on_lon = db.Column(db.Text)
-    on_lat = db.Column(db.Text)
-    on_geom = db.Column(Geometry(geometry_type='POINT', srid=2913))
-    on_stop_id = db.Column(db.Integer)
-    off_date = db.Column(db.DateTime)
-    off_lon = db.Column(db.Text)
-    off_lat = db.Column(db.Text)
-    off_geom = db.Column(Geometry(geometry_type='POINT', srid=2913))
-    off_stop_id = db.Column(db.Integer)
+    on_id = db.Column(db.Integer, db.ForeignKey("on_scan.id"), nullable=False)
+    #on_date = db.Column(db.DateTime)
+    #on_lon = db.Column(db.Text)
+    #on_lat = db.Column(db.Text)
+    #on_geom = db.Column(Geometry(geometry_type='POINT', srid=2913))
+    #on_stop_id = db.Column(db.Integer)
+    off_id = db.Column(db.Integer, db.ForeignKey("off_scan.id"), nullable=False)
+    #off_date = db.Column(db.DateTime)
+    #off_lon = db.Column(db.Text)
+    #off_lat = db.Column(db.Text)
+    #off_geom = db.Column(Geometry(geometry_type='POINT', srid=2913))
+    #off_stop_id = db.Column(db.Integer)
 
-    def __init__(self, line, dir, on_date, on_lon, on_lat, on_geom, on_stop_id,
-                                  off_date, off_lon, off_lat, off_geom, off_stop_id):
+    def __init__(self, line, dir, on_id, off_id):
         self.line = line
         self.dir = dir
-        self.on_date = on_date
-        self.on_lon = on_lon
-        self.on_lat = on_lat
-        self.on_geom = on_geom
-        self.on_stop_id = on_stop_id
-        self.off_date = off_date
-        self.off_lon = off_lon
-        self.off_lat = off_lat
-        self.off_geom = off_geom
-        self.off_stop_id = off_stop_id
+        self.on_id = on_id
+        self.off_id = off_id
+        #self.on_date = on_date
+        #self.on_lon = on_lon
+        #self.on_lat = on_lat
+        #self.on_geom = on_geom
+        #self.on_stop_id = on_stop_id 
+        #self.off_date = off_date
+        #self.off_lon = off_lon
+        #self.off_lat = off_lat
+        #self.off_geom = off_geom
+        #self.off_stop_id = off_stop_id
 
 class Stops(db.Model):
    __tablename__ = 'tm_stops'
@@ -128,66 +137,72 @@ class Routes(db.Model):
 
 db.create_all()
 
-@app.route('/submit', methods=['POST'])
-def post():
-    if request.method == 'POST':
-        uuid = request.form['uuid']
-        date = request.form['date']
-        line = request.form['line']
-        dir = request.form['dir']
-        lon = request.form['lon']
-        lat = request.form['lat']
-        mode = request.form['mode']
-        date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-        geom = None
-        stop_id = None 
-        try:
-            geom = ST_Transform(WKTElement('POINT('+lon+' '+lat+')',srid=4326),2913)
-            near_stop = Stops.query.filter_by(rte=int(line))\
-                             .order_by(Stops.geom.distance_box(geom))\
-                             .first()
-            stop_id = near_stop.stop_id
-        except:
-            app.logger.error("lat long could not be converted to geom or stopid not found")
-   
-    
-        add = None
-        if mode == 'on':
-            add = OnScan(uuid=uuid, date=date, line=line, dir=dir, lon=lon, lat=lat, geom=geom, stop_id=stop_id)
-        if mode == 'off':
-            on_scan = None
-            on_scan = OnScan.query.filter_by(uuid=uuid, line=line, dir=dir, match=False).order_by(OnScan.date.desc()).first()
-      
-            #TODO
-            """add logic checking for large time discrpency between on and off dates"""
-
-            if on_scan:
-                match = True
-                on_scan.match = True
-                pair = OnOff_Pairs(line=line, dir=dir,
-                                   on_date=on_scan.date,
-                                   on_lon=on_scan.lon,
-                                   on_lat=on_scan.lat,
-                                   on_geom = on_scan.geom,
-                                   on_stop_id = on_scan.stop_id,
-                                   off_date=date,
-                                   off_lon=lon,
-                                   off_lat=lat,
-                                   off_geom=geom,
-                                   off_stop_id=stop_id)
-                db.session.add(pair)
-            else:
-                match = False
-                app.logger.error("submit: off scan did not find matching on scan")
-                app.logger.error("        " + str(uuid) + " " + str(date))
-
-            add = OffScan(uuid=uuid, date=date, line=line, dir=dir, lon=lon, lat=lat, match=match, geom=geom, stop_id=stop_id)
-
-        db.session.add(add)
-        db.session.commit()
+def insertOn(params):
+    params = findStop(params)
+    geom = results["geom"]
+    stop_id = results["stop_id"]
+    insert = OnScan(uuid=uuid, date=date, line=line, dir=dir, lon=lon, lat=lat, geom=geom, stop_id=stop_id, dist=dist)
+    db.session.add(insert)
+    db.session.commit()
+ 
+def insertOff(params):
+    params = findStop(params)
+    on_scan = None
+    on_scan = OnScan.query.filter_by(uuid=params['uuid'], line=params['line'], dir=params['dir'], match=False)\
+                          .order_by(OnScan.date.desc()).first()
+    #TODO
+    """add logic checking for large time discrpency between on and off dates"""
+    if on_scan:
+        match = True
+        on_scan.match = True
+        pair = OnOff_Pairs(line=params['line'], dir=params['dir'], on_id = on_scan.id)
+        db.session.add(pair)
     else:
-        app.logger.error("request not post")
-  
+        match = False
+        app.logger.error("off scan did not find matching on scan")
+
+    insert = OffScan(uuid=params['uuid'], date=params['date'], line=params['line'], dir=params['dir'],
+                     match=match, geom=params['geom'], stop_id=params['stop_id'])
+    db.session.add(insert)
+    db.session.commit()
+
+def findStop(params):
+    geom = None
+    stop_id = None
+    try:
+        geom = ST_Transform(WKTElement('POINT('+params['lon']+' '+params['lat']+')',srid=4326),2913)
+        near_stop = Stops.query.filter_by(rte=int(params['line']))\
+                               .order_by(Stops.geom.distance_box(geom))\
+                               .first()
+        stop_id = near_stop.stop_id
+    except:
+        app.logger.error("lat long could not be converted to geom or stopid not found")
+    
+    params['geom'] = geom
+    params['stop_id'] = stop_id
+    return params 
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        
+        params = {}
+        params['uuid'] = request.form['uuid']
+        params['date'] = datetime.datetime.strptime(request.form['line'], "%Y-%m-%d %H:%M:%S")
+        params['line'] = request.form['line']
+        params['dir'] = request.form['dir']
+        params['lon'] = request.form['lon']
+        params['lat'] = request.form['lat']
+        mode = request.form['mode']
+        
+        if mode == 'on':
+            insertOn(params)
+        elif mode == 'off':
+            insertOn(params)
+    else:
+        app.logger.error("mode not 'on' or 'off'")
+    
     return "some response"
  
 @app.route('/stats')
@@ -234,20 +249,20 @@ def stats_filter(in_param):
 
 @app.route('/')
 def home():
-    return render_template('home.html', base_url=base_url)
+    return render_template('base.html', base_url=base_url)
 
 @app.route('/map')
 def map():
     geojson = {'type': 'FeatureCollection','features': []}
 
-    stops = db.session.query(func.ST_AsGeoJSON(func.ST_Transform(Stops.geom, 4326),10)).filter_by(rte=12,dir=0).all()
+    stops = db.session.query(func.ST_AsGeoJSON(func.ST_Transform(Stops.geom, 4326),10)).filter_by(rte=33,dir=0).all()
 
     for stop in stops:
         geom = ast.literal_eval(str(stop[0]))
         app.logger.error(stop.geom)
         feature_out = {'type':'Feature','geometry':geom,'properties':{'rte': '0'}}
         geojson['features'].append(feature_out)
-    return render_template('map.html', base_url=base_url, points=geojson)
+    return render_template('map.html', geojson=geojson)
 
 @app.route('/map/<rte>')
 def map_route(rte):
@@ -259,7 +274,6 @@ def map_route(rte):
         geojson['features'].append(feature_out)
     
     return render_template('map.html', base_url=base_url, points=geojson)
-
 
 if __name__ == '__main__':
     app.run()
