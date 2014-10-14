754ffb9ba3dc2a8a4ada0273d61bb9cf0418abdc
Jeff Meyers <meyersj@trimet.org>
2014-10-14 15:49:38 -0700

    working on report and downloads

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 
 from api import app
+
+downloads_dir = "/home/meyersj/api/app/api/static/downloads"
 
 @app.route('/')
 def index():
@@ -9,6 +11,16 @@ def index():
 @app.route('/tests')
 def tests():
     return render_template("report.html")
+
+
+@app.route('/downloads')
+def download_list():
+    return render_template("download.html")
+
+
+@app.route('/downloads/<filename>')
+def download(filename):
+    return send_from_directory(directory=downloads_dir, filename=filename)
 
 """
 @app.route('/report.html')
