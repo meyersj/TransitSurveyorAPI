5e7a9c61f9ee5c4ce6e087dbba6aae91ff36277d
Jeffrey Meyers <meyers2@pdx.edu>
2014-10-22 22:32:03 -0700

    working on status filtered by line

--- api/mod_onoff/helper.py
+++ api/mod_onoff/helper.py
@@ -14,12 +14,6 @@ INBOUND = '1'
 OUTBOUND = '0'
 DIRECTION = {'1':'Inbound', '0':'Outbound'}
 TRAINS = ['190','193','194','200']
-#QUOTAS = "/home/meyersj/api/app/data/route_quotas.csv"
-
-TARGETS = "data/pmlr_targets.csv"
-QUOTAS = os.path.join(app.config["ROOT_DIR"], "data/pmlr_targets.csv")
-
-
 
 # return total number of records for each route being surveyed
 def summary_status_query():
@@ -39,17 +33,6 @@ def summary_status_query():
         for record in scans_status.union(stops_status).order_by('rte')
     ]
     return status
-
-def quota(quota_file=None, route_field=None, target_field=None):
-    data = {}
-    if quota_file and route_field and target_field:
-        with open(quota_file, 'rb') as csv_file:
-            rows = csv.DictReader(csv_file)
-            for row in rows:
-                data[row[route_field]] = int(row[target_field])
-                #data[row[target_field]] = iint(row[target_field]) 
-    return data
-
 
 class Helper(object):
     targets = None
@@ -122,7 +105,7 @@ class Helper(object):
         for record in status:
             pct = round((float(record['count']) / float(record['target'])) * 100)
             categories.append(record['rte_desc'])
-            remaining.append(100 - pct) #record['target'] - record['count'])
+            remaining.append(100 - pct)
             complete.append(pct)
 
         series = [
@@ -154,211 +137,27 @@ class Helper(object):
 
     @staticmethod
     def get_routes():
-        routes = db.session.query(distinct(QuotasT.rte))
-        ret_val = [ route[0] for route in routes ]
-        #ret_val = [ str(rte) for rte in sorted(ret_val) ]
+        routes = db.session.query(
+            cast(QuotasT.rte, Integer).label('rte'),
+            QuotasT.rte_desc.label('rte_desc'))\
+            .distinct().order_by('rte')
+
+        ret_val = [ {'rte':str(route.rte), 'rte_desc':route.rte_desc}
+            for route in routes ]
         return ret_val
-
-class Quota(Helper):
-
-    #def __init__(self, quota_file=None)
-    #    super(Helper, self).__init__(quota_file=quota_file)
-
-    #def onoff(quot):
-    #    return quota(
-    #        quota_file=quotas_csv, route_field='rte',target_field='onoff_target')
-  
-    @staticmethod
-    def onoff(quotas_csv):
-        return quota(
-            quota_file=quotas_csv, route_field='rte',target_field='onoff_target')
-
  
     @staticmethod
-    def onoff(quotas_csv):
-        return quota(
-            quota_file=quotas_csv, route_field='rte',target_field='onoff_target')
+    def query_route(rte_desc):
+        targets = Helper.get_targets()
+        response = {}
+        for target in targets:
+            if target['rte_desc'] == rte_desc:
+                response[target['dir']] = {
+                    'dir_desc':target['dir_desc'],
+                    'target':target['target'],
+                    'complete':target['complete']
+                }
 
+        return response
 
-    #@staticmethod
-    #def onoff_targets(quotas_csv):
-    #    targets = target(quotas_csv, 'rte', 'onoff_target')
-    #    app.logger.debug(tarets) 
-    #    return targets
-
-
-    @staticmethod
-    def long(quotas_csv):
-        return quota(
-            quota_file=quotas_csv, route_field='rte',target_field='main_target')
-
-
-def percent(amount, total):
-    if total == 0:
-        return 100
-    else:
-        return round((float(amount) / total) * 100, 1)
-
-
-class Count(object):
-
-    @staticmethod
-    def records(**kwargs):
-        """Must have key 'line' in kwargs"""
-        count = None
-        if kwargs['line'] in TRAINS:
-            count = db.session.query(OnOffPairs_Stops)\
-                .filter_by(**kwargs)\
-                .count()
-        else:
-            count = db.session.query(OnOffPairs_Scans)\
-                .join(OnOffPairs_Scans.on)\
-                .filter_by(**kwargs)\
-                .count()
-        return count
-
-    @staticmethod
-    def complete():
-        routes = Query.routes()
-        
-        complete = 0
-        rem_count = 0
-        rem_total = 0
-        results = {}
-
-        quotas = Quota.onoff(QUOTAS)
-
-        for route in routes:
-            data = {}
-            count = Count.records(line=route)
-            quota = quotas[str(route)]
-
-            if count >= quota:
-                complete += 1
-            else:
-                rem_count += count
-                rem_total += quota
-        
-        results['complete'] = {'count':complete, 'rem':len(routes) - complete}
-        results['remaining'] = {'count':rem_count, 'rem':rem_total - rem_count}
-
-        return results
-
-class Chart(object):
-
-    @staticmethod
-    def single_route(line):
-        quotas = Quota.onoff(QUOTAS)
-
-        
-        in_total = quotas[str(line)]
-        in_count = Count.records(line=line, dir=INBOUND)
-        out_total = quotas[str(line)]
-        out_count = Count.records(line=line, dir=OUTBOUND)
-        #in_pct = percent(Count.records(line=line, dir=INBOUND), quotas[str(line)])
-        #out_pct = percent(Count.records(line=line, dir=OUTBOUND), quotas[str(line)])
-        categories = ['Inbound', 'Outbound']
-        series =  [
-            {'data':[in_total, out_total], 'name':'Remaining','color':RED_STATUS},
-            {'data':[in_count, out_count], 'name':'Complete', 'color':GREEN_STATUS}]
-        return {'series':series, 'categories':categories}
-
-    @staticmethod
-    def all_routes(routes):
-        complete = []
-    
-        quotas = Quota.onoff(QUOTAS)
-        
-        for route in routes:
-            #TODO fetch total from table instead of hard coded
-            pct = percent(Count.records(line=route), quotas[str(route)] * 2)
-            complete.append(pct)
-
-        categories = ['Route ' + str(route) for route in routes]
-        series = [
-            {'data': [100 - pct for pct in complete],'name':'Remaining', 'color':RED_STATUS},
-            {'data': complete,'name':'Complete', 'color':GREEN_STATUS}]
-        return {'series':series, 'categories':categories}
-
-
-
-TIME = "%H:%M"
-DATE = "%m-%d-%y"
-
-def date_time(on_date, off_date):
-    date = on_date.strftime(DATE)
-    time = on_date.strftime(TIME) + '-' + off_date.strftime(TIME)
-    return date, time
-
-class Query(object):
-    
-    @staticmethod
-    def records(**kwargs):
-        rows = []
-        
-        # fetch bus records
-        records = db.session.query(OnOffPairs_Scans)\
-            .join(OnOffPairs_Scans.off).filter_by(**kwargs)\
-            .order_by(desc(Scans.date), "line", "dir")\
-            .all()
-        
-        for r in records:
-            line = r.on.line
-            dir = r.on.dir
-            on_stop = r.on.stop_key.stop_name
-            off_stop = r.off.stop_key.stop_name
-            date, time = date_time(r.on.date, r.off.date)
-            user =  r.on.user_id + '/' + r.off.user_id
-            rows.append(
-                {'date':date,
-                 'time':time,
-                 'user':user,
-                 'line': line,
-                 'dir': DIRECTION[str(dir)],
-                 'on': on_stop,
-                 'off': off_stop})
-
-
-        # fetch train records 
-        records = db.session.query(OnOffPairs_Stops).filter_by(**kwargs)\
-            .order_by("date desc", "line", "dir")\
-            .all()
-        
-        for r in records:
-            line = r.line
-            dir = r.dir
-            on_stop = r.on.stop_name
-            off_stop = r.off.stop_name
-            date = r.date.strftime(DATE)
-            time = r.date.strftime(TIME)
-            user = r.user_id
-            rows.append(
-                {'date':date,
-                 'time':time,
-                 'user':user,
-                 'line': line,
-                 'dir': DIRECTION[str(dir)],
-                 'on': on_stop,
-                 'off': off_stop})
-
-        return rows
-
-    @staticmethod
-    def routes():
-        """returns sorted list of route numbers in database"""
-        routes = []
-        stops = db.session.query(OnOffPairs_Stops.line)\
-            .group_by(OnOffPairs_Stops.line)\
-            .order_by(OnOffPairs_Stops.line)\
-            .all()
-        scans = db.session.query(Scans.line)\
-            .group_by(Scans.line)\
-            .order_by(Scans.line)\
-            .all()
-
-        for s in scans:
-            routes.append(s.line)
-        for s in stops:
-            routes.append(s.line)
-        return routes
 
--- api/mod_onoff/views.py
+++ api/mod_onoff/views.py
@@ -5,15 +5,15 @@ from flask import Blueprint, redirect, url_for,render_template, jsonify, request
 from sqlalchemy import func
 
 from models import Scans, OnOffPairs_Scans, OnOffPairs_Stops
-from helper import Helper, Count, Chart, Query, summary_status_query
+from helper import Helper
 from api import app, db
 
 
 STATIC_DIR = '/onoff'
-INBOUND = '1'
-OUTBOUND = '0'
-DIRECTION = {'1':'Inbound', '0':'Outbound'}
-TRAINS = ['190','193','194','200']
+#INBOUND = '1'
+#OUTBOUND = '0'
+#DIRECTION = {'1':'Inbound', '0':'Outbound'}
+#TRAINS = ['190','193','194','200']
 
 
 mod_onoff = Blueprint('onoff', __name__, url_prefix='/onoff')
@@ -23,104 +23,54 @@ def static(html, static=STATIC_DIR):
     """returns correct path to static directory"""
     return os.path.join(static, html)
 
-
 @mod_onoff.route('/')
 def index():
-    #print url_for('onoff/overview')
-    return redirect(url_for('.onoff_overview'))
-
+    return redirect(url_for('.status'))
 
 @mod_onoff.route('/test')
 def test():
-     Helper.summary_status_query()
-     #quotas_file = os.path.join(app.config["ROOT_DIR"], "data/pmlr_targets.csv")
-     #helper = Helper(quota_file=quotas_file)
-     return ""
-
+    #Helper.summary_status_query()
+    #quotas_file = os.path.join(app.config["ROOT_DIR"], "data/pmlr_targets.csv")
+    #helper = Helper(quota_file=quotas_file)
+    return ""
 
 @mod_onoff.route('/overview')
 def onoff_overview():
-    results = Count.complete()
-    return render_template(static('overview.html'), results=results)
-
-
-def sort_targets(targets):
-    grouped = {}
-
-    for stats in targets:
-        rte = stats['rte']
-        rte_desc = stats['rte_desc']
-
-        data = {}
-        data['target'] = stats['target']
-        data['complete'] = stats['complete']
-        data['dir_desc'] = stats['dir_desc']
-
-        if rte not in grouped:
-            grouped[rte] = {}
-            grouped[rte]['rte_desc'] = stats['rte_desc']
-            grouped[rte]['data'] = {} 
-        grouped[rte]['data'][stats['dir']] = data
-        
-    sorted_rtes = sorted([int(key) for key in grouped.keys()])
-    targets_list = []
-    for rte in sorted_rtes:
-        rte = str(rte)
-        data = {
-            'rte':rte,
-            'rte_desc':grouped[rte]['rte_desc'],
-            '0':grouped[rte]['data']['0'],
-            '1':grouped[rte]['data']['1']
-        }
-        targets_list.append(data) 
-    
-    return targets_list
-
+    #results = Count.complete()
+    return ""#render_template(static('overview.html'), results=results)
 
 @mod_onoff.route('/status')
 def status():
-    helper = Helper()
-    #count = Helper.get_count()
-    #summary_status = Helper.summary_status_query()
+    routes = [ route['rte_desc'] for route in Helper.get_routes() ]
     chart = Helper.summary_chart()
-    print helper.routes
-    
-    #print chart
-    #print helper.routes
-    #print summary_status
-    # convert all route numbers to integer
-    # sort list then convert route numbers
-    # back to strings
-    #routes = [ int(rte) for rte in helper.routes ]
-    #routes = [ str(rte) for rte in sorted(set(route)) ]
-
-    #targets = sort_targets(helper.targets)
 
     return render_template(
-        static('status.html'), routes=helper.routes,
-        chart=chart
-    )
+        static('status.html'), routes=routes,chart=chart)
+
+
+#@mod_onoff.route('/status/_details', methods=['GET'])
+#def status_details():
+#     targets = Helper.get_targets()
+#     return jsonify(data=targets)
+
 
 
 @mod_onoff.route('/status/_details', methods=['GET'])
 def status_details():
-     helper = Helper()
-     return jsonify(data=helper.targets)
+     if 'rte_desc' in request.args.keys():
+         app.logger.debug(request.args['rte_desc'])
+         response = Helper.query_route(request.args['rte_desc'])
 
-
-"""
-@mod_onoff.route('/status/_details', methods=['GET'])
-def status_details():
-     if 'line' in request.args.keys():
-         line = request.args.get('line')
-         response = Chart.single_route(line)
+         #line = request.args.get('line')
+         #response = Chart.single_route(line)
      else:
-         routes = Query.routes()
-         response = Chart.all_routes(routes)
-     app.logger.debug(response)
+         pass
+         #routes = Query.routes()
+         #response = Chart.all_routes(routes)
+     #app.logger.debug(response)
      
-     return jsonify(**response)
-"""
+     return jsonify(response)
+
 
 @mod_onoff.route('/map')
 def map():
@@ -131,17 +81,17 @@ def map():
 def data():
     """Sets up table headers and dropdowns in template"""
     headers = ['Date', 'Time', 'User', 'Route', 'Direction', 'On Stop', 'Off Stop']
-    routes = Query.routes()
+    routes = [ route['rte_desc'] for route in Helper.get_routes() ]
     return render_template(static('data.html'), routes=routes, headers=headers)
 
 
 @mod_onoff.route('/data/_query', methods=['GET'])
 def data_query():
-    kwargs = {}
-    for key, value in request.args.iteritems():
-        kwargs[key] = value
-    records = Query.records(**kwargs)
-    return jsonify({'records':records})
+    #kwargs = {}
+    #for key, value in request.args.iteritems():
+    #    kwargs[key] = value
+    #records = Query.records(**kwargs)
+    return jsonify({})#{'records':records})
 
 
 
