68fb3563f78648d2cc204060327929f80bde5751
Jeff Meyers <meyersj@trimet.org>
2014-11-07 15:29:32 -0800

    add time of day table summary

--- api/mod_onoff/helper.py
+++ api/mod_onoff/helper.py
@@ -5,6 +5,7 @@ from sqlalchemy import func, desc, distinct, cast, Integer
 from flask import current_app
 #from api.shared.models import Scans, OnOffPairs_Scans, OnOffPairs_Stops
 from api import db, web_session
+from api import debug
 
 app = current_app
 
@@ -94,7 +95,9 @@ class Helper(object):
                 if stats:
                     categories.append(time + ": " + data[direction]['dir_desc'])
                     pct = 0
-                    if stats['count'] != 0:
+                    app.logger.debug(time)
+                    app.logger.debug(stats)
+                    if not (stats['count'] == 0 or stats['target'] == 0):
                         count = float(stats['count'])
                         target = float(stats['target'])
                         pct = round((count / target) * 100, 2)
@@ -112,38 +115,75 @@ class Helper(object):
     @staticmethod
     def get_routes():
         ret_val = []
-
         routes = web_session.execute("""
             SELECT rte, rte_desc
             FROM v.lookup_rte
             ORDER BY rte;""")
 
-        ret_val = [ {'rte':str(route[0]), 'rte_desc':route[1]}
+        RTE = 0
+        RTE_DESC = 1
+        ret_val = [ {'rte':str(route[RTE]), 'rte_desc':route[RTE_DESC]}
             for route in routes ]
         
         return ret_val
 
     @staticmethod
-    def query_route_data(rte_desc):
+    def get_directions():
         ret_val = []
-      
+        directions = web_session.execute("""
+            SELECT rte, rte_desc, dir, dir_desc
+            FROM v.lookup_dir
+            ORDER BY rte, dir;""")
+
+        RTE = 0
+        RTE_DESC = 1
+        DIR = 2
+        DIR_DESC = 3
+
+        ret_val = [ {'rte':str(direction[RTE]), 'rte_desc':direction[RTE_DESC],
+            'dir':int(direction[DIR]), 'dir_desc':direction[DIR_DESC]}
+            for direction in directions ]
+        app.logger.debug(ret_val)
+
+        return ret_val
+
+
+
+
+    @staticmethod
+    def query_route_data(rte_desc='', dir_desc=''):
+        ret_val = []
+        debug("query: " + rte_desc + ' ' + dir_desc)
+
         # query last 100 most recent
-        # records for route passed in
-        
-        if rte_desc and rte_desc != 'All': 
+        # records for route and direction passed in
+        # TODO method to build query instead of having three..
+        if rte_desc and dir_desc:
+            debug('rte and dir')
             query = web_session.execute("""
                 SELECT rte_desc, dir_desc, date, time, user_id,
                     on_stop_name, off_stop_name
                 FROM v.display_data
                 WHERE rte_desc = :rte_desc
-                ORDER BY date, time DESC
-                LIMIT 100;""", {'rte_desc':rte_desc})
-        else:
+                AND dir_desc = :dir_desc
+                ORDER BY date DESC, time DESC
+                LIMIT 100;""", {'rte_desc':rte_desc, 'dir_desc':dir_desc})
+        elif rte_desc:
+            debug('just_dir')
             query = web_session.execute("""
                 SELECT rte_desc, dir_desc, date, time, user_id,
                     on_stop_name, off_stop_name
                 FROM v.display_data
-                ORDER BY date, time DESC
+                WHERE rte_desc = :rte_desc
+                ORDER BY date DESC, time DESC
+                LIMIT 100;""", {'rte_desc':rte_desc})
+        else:
+            debug('other')
+            query = web_session.execute("""
+                SELECT rte_desc, dir_desc, date, time, user_id,
+                    on_stop_name, off_stop_name
+                FROM v.display_data
+                ORDER BY date DESC, time DESC
                 LIMIT 100;""")
 
         RTE_DESC = 0
@@ -157,7 +197,6 @@ class Helper(object):
         # each record will be converted as json
         # and sent back to page
         for record in query:
-            app.logger.debug(record)
             data = {}
             data['date'] = str(record[DATE])
             data['time'] = str(record[TIME])
@@ -167,68 +206,136 @@ class Helper(object):
             data['on_stop'] = record[ON_STOP]
             data['off_stop'] = record[OFF_STOP]
             ret_val.append(data)
+      
+        debug(ret_val)
+        return ret_val
+
+
+    @staticmethod
+    def query_route_status(rte_desc=''):
+        # set rte_desc to wildcard to query
+        # if no route was specified
+        ret_val = {}
+      
+        # query web database
+        # using helper views
+        
+        if rte_desc:
+            query = web_session.execute("""
+                SELECT rte, rte_desc, dir, dir_desc,
+                    time_period, count, target
+                FROM v.summary
+                WHERE rte_desc = :rte_desc
+                ORDER BY rte, dir,
+                    CASE time_period
+                        WHEN 'AM Peak' THEN 1
+                        WHEN 'Midday' THEN 2
+                        WHEN 'PM Peak' THEN 3
+                        WHEN 'Evening' THEN 4
+                        WHEN 'Total' THEN 5
+                    ELSE 6
+                    END;""", {'rte_desc':rte_desc})
+            ret_val = Helper.build_response_route_status(query)
+
+        else:
+            # query web database
+            # using helper views
+            query = web_session.execute("""
+                SELECT rte, rte_desc, dir, dir_desc,
+                    time_period, count, target
+                FROM v.summary
+                ORDER BY rte, dir,
+                    CASE time_period
+                        WHEN 'AM Peak' THEN 1
+                        WHEN 'Midday' THEN 2
+                        WHEN 'PM Peak' THEN 3
+                        WHEN 'Evening' THEN 4
+                        WHEN 'Total' THEN 5
+                    ELSE 6
+                    END;""")
+            ret_val = Helper.build_response_summary_status(query)
+          
+       
        
         return ret_val
 
     @staticmethod
-    def query_route_status(rte_desc):
+    def build_shell(dir_desc):
         ret_val = {}
-       
-        # query web database
-        # using helper views
-        query = web_session.execute("""
-            SELECT rte, rte_desc, dir, dir_desc,
-                time_period, count, target
-            FROM v.summary
-            WHERE rte_desc = :rte_desc
-            ORDER BY rte, dir,
-                CASE time_period
-                    WHEN 'AM Peak' THEN 1
-                    WHEN 'Midday' THEN 2
-                    WHEN 'PM Peak' THEN 3
-                    WHEN 'Evening' THEN 4
-                    WHEN 'Total' THEN 5
-                ELSE 6
-                END;""", {'rte_desc':rte_desc})
-        
-        # index for each tuple in query results
-        RTE = 0
-        RTE_DESC = 1
-        DIR = 2
-        DIR_DESC = 3
-        TIME = 4
-        COUNT = 5
-        TARGET = 6
+        ret_val['dir_desc'] = dir_desc
+        ret_val['AM Peak'] = {}
+        ret_val['Midday'] = {}
+        ret_val['PM Peak'] = {}
+        ret_val['Evening'] = {}
+        return ret_val
 
-        def build_shell(dir_desc):
-            ret_val = {}
-            ret_val['dir_desc'] = dir_desc
-            ret_val['AM Peak'] = {}
-            ret_val['Midday'] = {}
-            ret_val['PM Peak'] = {}
-            ret_val['Evening'] = {}
-            return ret_val
+
+    @staticmethod
+    def build_response_summary_status(query):
+        ret_val = {}
+        
+        for record in query:
+            rte = int(record[0])
+            rte_desc = record[1]
+            dir = int(record[2])
+            dir_desc = record[3]
+            time = record[4]
+            count = int(record[5])
+            target = int(record[6])
+
+            data = {}
+            data['target'] = target
+            data['count'] = count
+
+            if rte_desc not in ret_val:
+                ret_val[rte_desc] = {}
+
+            # set up each time period of this direction
+            # populate later when that record is fetched
+            if str(dir) not in ret_val[rte_desc]:
+                ret_val[rte_desc][str(dir)] = Helper.build_shell(dir_desc)
+   
+            if target == 0:
+                data = {}
+
+            ret_val[rte_desc][str(dir)][time] = data
+        
+        return ret_val
+    
+    @staticmethod
+    def build_response_route_status(query):
+        ret_val = {}
 
         # look through query results
         # and build response
         for record in query:
-            str_dir = str(record[DIR])
+            rte = int(record[0])
+            rte_desc = record[1]
+            dir = int(record[2])
+            dir_desc = record[3]
+            time = record[4]
+            count = int(record[5])
+            target = int(record[6])
+            
             data = {}
-            data['target'] = int(record[TARGET])
-            data['count'] = int(record[COUNT])
+            data['target'] = target
+            data['count'] = count
 
             # add route description
             # only executes in first loop
-            if record[RTE_DESC] not in ret_val:
-                ret_val['rte_desc'] = record[RTE_DESC]
+            if 'rte_desc' not in ret_val:
+                ret_val['rte_desc'] = rte_desc
             
             # build dictionary for each time period
-            if str_dir not in ret_val:
-                ret_val[str_dir] = build_shell(record[DIR_DESC])
+            if str(dir) not in ret_val:
+                ret_val[str(dir)] = Helper.build_shell(dir_desc)
             
             # set target and count data into correct
             # direction and time period
-            ret_val[str_dir][record[TIME]] = data
-       
+            if target == 0:
+                data = {}
+            
+            ret_val[str(dir)][time] = data
+        
         return ret_val
 
--- api/mod_onoff/views.py
+++ api/mod_onoff/views.py
@@ -7,7 +7,7 @@ from sqlalchemy import func
 from models import Scans, OnOffPairs_Scans, OnOffPairs_Stops
 from helper import Helper
 from api import app, db
-
+from api import debug, error, web_session
 
 STATIC_DIR = '/onoff'
 mod_onoff = Blueprint('onoff', __name__, url_prefix='/onoff')
@@ -19,24 +19,48 @@ def static(html, static=STATIC_DIR):
 
 @mod_onoff.route('/')
 def index():
-    return redirect(url_for('.status'))
+    return redirect(url_for('.overview'))
 
 @mod_onoff.route('/test')
 def test():
     return "hilogg"
-    
 
+"""
 @mod_onoff.route('/overview')
 def overview():
     #results = Count.complete()
     return render_template(static('base.html'))
     #return "overview"#render_template(static('overview.html'), results=results)
+"""
+
+@mod_onoff.route('/table_status')
+def overview():
+    routes = [ route['rte_desc'] for route in Helper.get_routes() ]
+    data = Helper.query_route_status()
+    query = web_session.execute("""
+        SELECT rte_desc, sum(count) AS count
+        FROM v.records
+        WHERE rte_desc LIKE 'Portland Streetcar%'
+        GROUP by rte_desc;""")
+    
+    streetcar = {
+            "Portland Streetcar - NS Line":{'target':2182, 'count':0},
+            "Portland Streetcar - CL Line":{'target':766, 'count':0}
+    }
+    
+    for record in query:
+        debug(record)
+        streetcar[record[0]]['count'] = int(record[1])
+    
+    return render_template(static('table_status.html'), 
+            streetcar=streetcar, routes=routes, data=data)
+
 
 @mod_onoff.route('/status')
 def status():
     routes = [ route['rte_desc'] for route in Helper.get_routes() ]
     chart = Helper.summary_chart()
-
+    #streetcar = {"Portland Streetcar - NS Line":{"target
     return render_template(
         static('status.html'), routes=routes,chart=chart)
 
@@ -52,7 +76,7 @@ def status_details():
      response = {'success':False}
      
      if 'rte_desc' in request.args.keys():
-         data = Helper.query_route_status(request.args['rte_desc'])
+         data = Helper.query_route_status(rte_desc=request.args['rte_desc'])
          chart = Helper.single_chart(data)
          response['success'] = True
          response['data'] = data
@@ -69,18 +93,32 @@ def data():
     """Sets up table headers and dropdowns in template"""
     headers = ['Date', 'Time', 'User', 'Route', 'Direction', 'On Stop', 'Off Stop']
     routes = [ route['rte_desc'] for route in Helper.get_routes() ]
-    return render_template(static('data.html'), routes=routes, headers=headers)
+    
+    directions = Helper.get_directions()
+
+    # build response
+    #for direction in directions:
+    #directions = [ direction['dir_desc'] for direction in Helper.get_directions() ]
+
+    return render_template(static('data.html'),
+            routes=routes, directions=directions, headers=headers)
 
 
 @mod_onoff.route('/data/_query', methods=['GET'])
 def data_query():
     response = []
     rte_desc = ""
+    dir_desc = ""
+
     if 'rte_desc' in request.args.keys():
-        rte_desc = request.args['rte_desc']
-        response = Helper.query_route_data(rte_desc)
+        rte_desc = request.args['rte_desc'].strip()
+    if 'dir_desc' in request.args.keys():
+        dir_desc = request.args['dir_desc'].strip()
+        debug("dir desc: " + dir_desc)
+
+    response = Helper.query_route_data(rte_desc=rte_desc, dir_desc=dir_desc)
         
-    return jsonify(data=response, rte_desc=rte_desc)
+    return jsonify(data=response)
 
 
 
