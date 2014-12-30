3f5067c1992b673c3a78928b93fdfdddac89cfb1
Jeffrey Meyers <meyers2@pdx.edu>
2014-12-29 21:06:57 -0800

    map offs

--- api/mod_onoff/views.py
+++ api/mod_onoff/views.py
@@ -62,22 +62,6 @@ def status():
     return render_template(static('status.html'), 
             streetcar=streetcar, routes=routes, data=data, summary=summary)
 
-"""
-
-@mod_onoff.route('/old_status')
-def status():
-    routes = [ route['rte_desc'] for route in Helper.get_routes() ]
-    chart = Helper.summary_chart()
-    #streetcar = {"Portland Streetcar - NS Line":{"target
-    return render_template(
-        static('status.html'), routes=routes,chart=chart)
-"""
-
-#@mod_onoff.route('/status/_details', methods=['GET'])
-#def status_details():
-#     targets = Helper.get_targets()
-#     return jsonify(data=targets)
-
 
 @mod_onoff.route('/status/_details', methods=['GET'])
 def status_details():
@@ -92,9 +76,6 @@ def status_details():
      
      return jsonify(response)
 
-#@mod_onoff.route('/map')
-#def map():
-#    return render_template(static('map.html'))
 
 @mod_onoff.route('/data')
 def data():
@@ -167,6 +148,8 @@ def map():
     return render_template(static('map.html'),
         routes=routes, directions=directions
     )
+
+
 
 @mod_onoff.route('/map/_details', methods=['GET'])
 def map_details():
@@ -246,4 +229,94 @@ def map_details():
         session.close()
     return jsonify(response)
 
+
+@mod_onoff.route('/map_apc')
+def map_apc():
+    routes = [ route['rte_desc'] for route in h.get_routes() ]
+    directions = h.get_directions()
+    return render_template(static('map_apc.html'),
+        routes=routes, directions=directions
+    )
+
+@mod_onoff.route('/map_apc/_details', methods=['GET'])
+def map_apc_details():
+    response = {'success':False}
+    if 'rte_desc' in request.args.keys():
+        rte_desc = request.args['rte_desc'].strip()
+        rte = h.rte_lookup(rte_desc)
+        session = Session()
+        
+        
+        #fields = ['dir', 'tad', 'centroid', 'stops', 'ons', 'count']
+        #query = session.execute("""
+        #    SELECT """ + ','.join(fields) + """
+        #    FROM long.tad_stats
+        #    WHERE rte = :rte;""", {'rte':rte})
+        #query_time = session.execute("""
+        #    SELECT """ + ','.join(fields) + """, bucket
+        #    FROM long.tad_time_stats
+        #    WHERE rte = :rte;""", {'rte':rte})
+        #query_routes = session.execute("""
+        #    SELECT dir, ST_AsGeoJson(ST_Transform(ST_Union(geom), 4326))
+        #    FROM tm_routes
+        #    WHERE rte = :rte
+        #    GROUP BY dir;""", {'rte':rte})
+        #query_minmax = session.execute("""
+        #    SELECT dir, label, stop_name, ST_AsGeoJson(ST_Transform(geom, 4326))
+        #    FROM long.stop_minmax
+        #    WHERE rte = :rte;""", {'rte':rte})
+        """
+        def build_data(record):
+            data = {}
+            for index in range(1, len(fields)):
+                field = record[index]
+                if isinstance(field, Decimal): field = int(field)
+                data[fields[index]] = field
+            return data
+
+        def int_zero(value):
+            try:
+                return int(value)
+            except:
+                return False
+
+        data = {}
+        data[0] = []
+        data[1] = []
+        time_data = {}
+        time_data[0] = {}
+        time_data[1] = {}
+        routes_geom = {}
+        minmax = {} 
+        for record in query:
+            data[record[0]].append(build_data(record))
+        for record in query_time:
+            tad = record[1]
+            if tad not in time_data[record[0]]:
+                time_data[record[0]][tad] = []
+            ons = int_zero(record[4])
+            count = int_zero(record[5])
+            bucket = int_zero(record[6])
+            time_data[record[0]][tad].insert(bucket, {"count":count, "ons":ons})
+        for record in query_routes:
+            routes_geom[record[0]] = {
+                'dir':record[0],
+                'geom':json.loads(record[1])
+            }
+        for record in query_minmax:
+            if record[0] not in minmax:
+                minmax[record[0]] = {}
+            minmax[record[0]][record[1]] = {
+                'geom':record[3],
+                'stop_name':record[2]
+            }
+        response['success'] = True
+        response['data'] = data
+        response['time_data'] = time_data
+        response['routes'] = routes_geom
+        response['minmax'] = minmax
+        """
+        
+        session.close()
+    return jsonify(response)
 
