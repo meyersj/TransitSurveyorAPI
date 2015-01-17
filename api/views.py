0ea788527c887c16bf6fd00defb2d485bc6ae17d
Jeff Meyers <meyersj@trimet.org>
2015-01-17 12:07:42 -0800

    cleanup api base views

--- api/views.py
+++ api/views.py
@@ -1,29 +1,8 @@
-from flask import render_template, send_from_directory
+from flask import render_template
 
 from api import app
-
-downloads_dir = "/home/meyersj/api/app/api/static/downloads"
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/tests')
-def tests():
-    return render_template("report.html")
-
-
-@app.route('/downloads')
-def download_list():
-    return render_template("download.html")
-
-
-@app.route('/downloads/<filename>')
-def download(filename):
-    return send_from_directory(directory=downloads_dir, filename=filename)
-
-"""
-@app.route('/report.html')
-def report():
-    return render_template("report.html")
-"""
