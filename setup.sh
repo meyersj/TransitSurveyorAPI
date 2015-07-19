41edb015139870b53847f58d44ce46ec61c387e4
Jeffrey Meyers <jeffrey.alan.meyers@gmail.com>
2015-07-19 15:21:48 -0700

    Update setup.sh
    add server param for hostname or ip

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+server=$1
 user=survey
 db=${user}
 db_user=${db}
@@ -24,17 +25,22 @@ su - postgres -c "createdb -O ${db_user} ${db}"
 su - postgres -c "psql -c \"CREATE EXTENSION postgis;\" ${db}"
 
 # clone API repo, build database tables and setup environment
-su - ${user} # switch to survey user
 su - ${user} -c "git clone https://github.com/TransitSurveyor/API"
 su - ${user} -c "psql -f ${proj}/db/schema.sql -d ${db}"
 su - ${user} -c "psql -f ${proj}/db/stops.sql -d ${db}"
 su - ${user} -c "virtualenv ${proj}/env"
 su - ${user} -c "${proj}/env/bin/pip install -r ${proj}/requirements.txt"
 
+
+
 cp ${proj}/nginx_site_config /etc/nginx/sites-available/api
 cp ${proj}/upstart_init_conf /etc/init/api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/api /etc/nginx/sites-enabled/api
+
+if [ "${server}" -gt "-1" ]
+  then sed -i 's/host_name_or_ip/${server}/' /etc/nginx/sites-available/api
+fi
 
 # open up nginx and upstart configs and verify the paths and server_name ...
 #   vim /etc/nginx/sites-available/api
